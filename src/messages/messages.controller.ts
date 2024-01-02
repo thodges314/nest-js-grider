65e823d4281de99703d0bb9eadde449b3c8b1424
Thomas Hodges <thodges@javascriptonline.org>
2024-01-01 22:13:31 -0800

    example repository/service setup w/controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // constructor is creating it's own dependencies
+    // DONT DO THIS ON REAL APPS
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
